2a92972f913c103d960987024d6ffb19d2ab7934
spezdmtest <spezd.m.@gmail.com>
2021-03-20 09:34:26 +0200

    Collection and Multithreading

---
+++ src/Multithreading/Main_21_Phaser.java
@@ -0,0 +1,27 @@
+package Multithreading;
+
+import java.util.concurrent.Phaser;
+
+public class Main_21_Phaser {
+    public static void main(String[] args) {
+        Phaser phaser = new Phaser(2);
+        new Washer(phaser);
+        new Washer(phaser);
+    }
+    static class Washer extends Thread {
+        Phaser phaser;
+
+        public Washer(Phaser phaser) {
+            this.phaser = phaser;
+            start();
+        }
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 3; i++) {
+                System.out.println(getName() + " washing the car");
+                phaser.arriveAndAwaitAdvance();
+            }
+        }
+    }
+}
