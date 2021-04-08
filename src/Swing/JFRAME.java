34ed0faa1fb90b09bb25258fa3fa65c08d11119f
spezdmtest <spezd.m.@gmail.com>
2021-04-08 11:39:28 +0300

    Swing

---
+++ src/Swing/JFRAME.java
@@ -0,0 +1,19 @@
+package Swing;
+import javax.swing.*;
+import java.awt.*;
+
+public class JFRAME {
+    public static void main(String[] args) {
+        ImageIcon img = new ImageIcon("C:\\Users\\spezdm\\IdeaProjects\\AXYYR\\src\\Swing\\java.png");
+        JFrame jFrame = new JFrame() {};
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        //jFrame.setSize(500,300);
+        //jFrame.setLocation(600,400);
+        jFrame.setBounds(dimension.width/2-250,dimension.height/2-200,500,400);
+        jFrame.setTitle("this my app");
+        jFrame.setIconImage(img.getImage());
+    }
+}
